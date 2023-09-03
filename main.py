37c821fbe33c9038a863121ebd46743288e9eddc
devporter007 <portzero7@protonmail.com>
2023-09-03 18:32:29 +0530

    fi

--- main.py
+++ main.py
@@ -14,17 +14,11 @@ COLORS = {
     "cyan-background": "\u001b[46;1m",
 }
 
-
-def colortext(text):
-    for color in COLORS:
-        text = text.replace("[[" + color + "]]", COLORS[color])
-    return text
-
-
 ver = "0.1"
+
 gen = f'''
                                                 [[yellow]]Port007[[white]] proudly presents:
-                                                     
+
                                             [[red]]     _\\__o__ __o/   o__ __o    
                                                       v    |/  /v     v\\   
                                                           /   />       <\\  
@@ -34,14 +28,26 @@ gen = f'''
                                                     o/        \\         /  
                                                    /v          o       o   
                                                   />           <\\__ __/>   
-                                                                           
+
                                                          [[cyan]]{ver}[[white]]
-                                                         
+
                               [[magenta]]      Making tools to create archive grade releases since 2023     
-                                      
+
                     This tool is part of a tool pack made to archive art of movies for the uncertain future[[white]]
 '''
-print(colortext(gen))
+
+
+
+def colortext(text):
+    for color in COLORS:
+        text = text.replace("[[" + color + "]]", COLORS[color])
+    return text
+def Clear():
+    os.system('cls')
+    print(colortext(gen))
+
+Clear()
+
 try:
     buddy = sys.argv[1]
     try:
@@ -55,14 +61,18 @@ try:
     extensionlessname = medname[:-4]
     print("Step-1 : Extracting RPU and removing EL....")
     os.system(f"ffmpeg -y -i {buddy} -dn -c:v copy -vbsf hevc_mp4toannexb -f hevc - | dovi_tool -m 2 convert --discard - -o {workdir}\\discarded.hevc")
+    Clear()
     print("Step-2: Genering mka without video keeping everything else intact....")
     print("Do this yourself in MKVToolNiX GUI and press enter, this step is WIP")
     input("")
+    Clear()
     print("Step-3: Combining shit...")
     os.system(f"mkvmerge -o {workdir}{extensionlessname}.P8.mkv {workdir}\\discarded.hevc {workdir}{extensionlessname}.mka")
+    Clear()
     print("Step-4: Cleaning up shit")
     os.system(f"del {workdir}\\discarded.hevc")
     os.system(f"del {workdir}{extensionlessname}.mka")
+    Clear()
     print("Hola, signing off....")
 except IndexError:
     print("No inputs found, please drag the file to this binary.")
